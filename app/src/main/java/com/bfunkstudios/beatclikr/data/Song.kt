dd7983be6832fff6b3c6947e2e3cc3660bdfb3f6
Ben Funk <bfunk1978@icloud.com>
2026-04-21 06:05:39 -0500

    use song playback model like iOS app

--- app/src/main/java/com/bfunkstudios/beatclikr/data/Song.kt
+++ app/src/main/java/com/bfunkstudios/beatclikr/data/Song.kt
@@ -2,12 +2,25 @@ package com.bfunkstudios.beatclikr.data
 
 import java.util.UUID
 
-data class Song(var title: String,
-                var artist: String,
-                var beatsPerMinute: Float,
-                var beatsPerMeasure: Int,
-                var subdivisions: Subdivisions,
-                var liveSequence: Int?,
-                var rehearsalSequence: Int?,
-                val id: UUID = UUID.randomUUID()) {
+data class Song(
+    var title: String,
+    var artist: String,
+    var beatsPerMinute: Float,
+    var beatsPerMeasure: Int,
+    var subdivisions: Subdivisions,
+    var liveSequence: Int?,
+    var rehearsalSequence: Int?,
+    val id: UUID = UUID.randomUUID()
+) {
+    companion object {
+        fun instantSong() = Song(
+            title = "Instant",
+            artist = "Song",
+            beatsPerMinute = 120f,
+            beatsPerMeasure = 4,
+            subdivisions = Subdivisions.Quarter,
+            liveSequence = null,
+            rehearsalSequence = null
+        )
+    }
 }
