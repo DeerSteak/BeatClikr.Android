f4a44efffbb7cbc9ff371d37bc63f5c2654f381d
Ben Funk <bfunk1978@icloud.com>
2026-05-16 08:12:40 -0500

    fix warning about StringRes

--- app/src/main/java/com/bfunkstudios/beatclikr/data/SoundBank.kt
+++ app/src/main/java/com/bfunkstudios/beatclikr/data/SoundBank.kt
@@ -3,7 +3,7 @@ package com.bfunkstudios.beatclikr.data
 import androidx.annotation.StringRes
 import com.bfunkstudios.beatclikr.R
 
-enum class SoundBank(@StringRes val labelRes: Int) {
+enum class SoundBank(@param:StringRes val labelRes: Int) {
     ACOUSTIC(R.string.settings_low_latency_sounds_acoustic),
     SYNTH(R.string.settings_low_latency_sounds_synth)
 }
